f72e5187faf49f620190884f0c5c5b44450aa3b1
UalquerRamosToledo <ualquertoledo.aluno@unipampa.edu.br>
2023-01-18 23:01:26 -0300

    Carro está cadastrando

--- src/InterfacesGraficas/Veiculos/AddCarroFrame.java
+++ src/InterfacesGraficas/Veiculos/AddCarroFrame.java
@@ -1,5 +1,6 @@
 package InterfacesGraficas.Veiculos;
 
+import Fonte.Carro;
 import Fonte.RelacaoDeVeiculos;
 
 import javax.swing.*;
@@ -8,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddCarroFrame extends JFrame {
-
-    //String placa, int ano, double valor, int passageiros, int portas, double quilometros, boolean arCondicionado
     private JLabel placaLabel, anoLabel, valorLabel, passageirosLabel, portasLabel, quilometrosLabel, arCondicionadoLabel;
     private JTextField placaField, anoField, valorField, passageirosField, portasField, quilometrosField, arCondicionadoField;
     private JButton btnCadastrar, btnLimpar;
@@ -21,34 +20,38 @@ public class AddCarroFrame extends JFrame {
         this.relacaoDeVeiculos = relacaoDeVeiculos;
         setSize(1200, 800);
         setLocationRelativeTo(null);
-        setLayout( new FlowLayout());
 
         JPanel addCarroPanel = new JPanel();
 
-        placaLabel = new JLabel("Modelo:");
-
-        placaField = new JTextField(20);
-        anoLabel = new JLabel("Marca:");
-        anoField = new JTextField(20);
-        valorLabel = new JLabel("Ano:");
-        valorField = new JTextField(20);
-        passageirosLabel = new JLabel("Cor:");
-        passageirosField = new JTextField(20);
-
+        placaLabel = new JLabel("Placa:");
+        placaField = new JTextField(10);
+        anoLabel = new JLabel("Ano:");
+        anoField = new JTextField(10);
+        valorLabel = new JLabel("Valor:");
+        valorField = new JTextField(10);
+        passageirosLabel = new JLabel("Capacidade de Passageiros:");
+        passageirosField = new JTextField(10);
+        portasLabel = new JLabel("Número de Portas:");
+        portasField = new JTextField(10);
+        quilometrosLabel = new JLabel("Quilometros Rodados:");
+        quilometrosField = new JTextField(10);
+        jRadioButton = new JRadioButton("Ar condicionado:");
+        jRadioButton.setSelected(false);
 
         btnCadastrar = new JButton("Cadastrar");
         btnLimpar = new JButton("Limpar");
 
         btnCadastrar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                //String placa, int ano, double valor, int passageiros, int portas, double quilometros, boolean arCondicionado
-                //colocar tudo em variaveis string e criar o eiuclo e add e lipar os campos e setar a visibulidade
                 String placa = placaField.getText();
                 int ano = Integer.parseInt(anoField.getText());
                 double valor = Double.parseDouble(valorField.getText());
                 int passageiros = Integer.parseInt(passageirosField.getText());
                 int portas = Integer.parseInt(portasField.getText());
                 double quilometros = Double.parseDouble(quilometrosField.getText());
+                boolean arCondicionado = jRadioButton.isSelected();
+                Carro carro = new Carro(placa,ano,valor,passageiros,portas,quilometros,arCondicionado);
+                relacaoDeVeiculos.add(carro);
 
 
             }
@@ -62,7 +65,7 @@ public class AddCarroFrame extends JFrame {
                 passageirosField.setText("");
             }
         });
-
+        // int portas, double quilometros, boolean arCondicionado
         addCarroPanel.add(placaLabel);
         addCarroPanel.add(placaField);
         addCarroPanel.add(anoLabel);
@@ -71,8 +74,13 @@ public class AddCarroFrame extends JFrame {
         addCarroPanel.add(valorField);
         addCarroPanel.add(passageirosLabel);
         addCarroPanel.add(passageirosField);
-        addCarroPanel.add(btnCadastrar);
+        addCarroPanel.add(portasLabel);
+        addCarroPanel.add(portasField);
+        addCarroPanel.add(quilometrosLabel);
+        addCarroPanel.add(quilometrosField);
+        addCarroPanel.add(jRadioButton);
         addCarroPanel.add(btnLimpar);
+        addCarroPanel.add(btnCadastrar);
         add(addCarroPanel, BorderLayout.CENTER);
 
         setVisible(true);
